66fdb3c28d950746ce7ffba39aa1df18f5818aa7
Samduykhanh01 <samduykhanh01@gmail.com>
2024-08-07 16:43:06 -0400

    Update Neovim configuration and data

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,18 @@ vim.keymap.set(
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "Resume" }
 )
+
+-- Move line up
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+
+-- Move line down
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+
+-- Insert a new line below and move to it
+--[[vim.keymap.set(
+  "i",
+  "<C-S-CR>",
+  '<Esc>:echo "Ctrl + Shift + Enter pressed in Insert mode"<CR>',
+  { noremap = true, silent = true }
+)
+--]]
