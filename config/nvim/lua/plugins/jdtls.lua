07ce2b375fda0dba18beb6e707786100784bb921
Samduykhanh01 <samduykhanh01@gmail.com>
2024-08-13 12:56:20 -0400

    Update Neovim configuration and data

--- config/nvim/lua/plugins/jdtls.lua
+++ config/nvim/lua/plugins/jdtls.lua
@@ -17,22 +17,48 @@ return {
 }
 --]]
 
+--[[
+        home .. "/.local/share/nvim/mason/bin/jdtls", -- Path to JDTLS executable
+        "-data",
+        home .. "/.cache/jdtls/workspace/", -- Workspace directory for JDTLS
+        --]]
+
 return {
   "mfussenegger/nvim-jdtls",
   dependencies = { "folke/which-key.nvim" },
   opts = function()
     local home = os.getenv("HOME")
+    local mason_path = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+    local workspace_folder = home .. "/.cache/jdtls/workspace/"
+    local root_dir = require("jdtls.setup").find_root({ "src", ".git" })
 
-    -- Define the command to start JDTLS with required options
     local config = {
+      -- Define the command to start JDTLS with required options
       cmd = {
-        home .. "/.local/share/nvim/mason/bin/jdtls", -- Path to JDTLS executable
+        -- "/usr/lib/jvm/java-18-openjdk-amd64/",
+
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        mason_path .. "/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar",
+        "-configuration",
+        mason_path .. "/config_linux",
         "-data",
-        home .. "/.cache/jdtls/workspace/", -- Workspace directory for JDTLS
+        workspace_folder,
       },
 
       -- Use nvim-lspconfig utilities to determine the project root
-      root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+      root_dir = root_dir,
 
       -- Project-specific settings
       settings = {
@@ -41,7 +67,38 @@ return {
             sourcePaths = { "java/src", "src", "java" }, -- Add your source paths here
           },
           -- Additional Java settings can be added here
+
+          eclipse = {
+            downloadSources = true,
+          },
+          maven = {
+            downloadSources = true,
+          },
+
+          implementationsCodeLens = {
+            enabled = true,
+          },
+          referencesCodeLens = {
+            enabled = true,
+          },
+
+          references = {
+            includeDecompiledSources = true,
+          },
+          format = {
+            enabled = true,
+          },
         },
+      },
+
+      -- Specify classpath manually
+      classpaths = {
+        vim.fn.glob(root_dir .. "/lib/*.jar", true, 1), -- Add all JARs in the lib directory
+      },
+
+      -- Specify source directories manually
+      sourcepaths = {
+        root_dir .. "/src",
       },
 
       -- Additional capabilities for JDTLS
