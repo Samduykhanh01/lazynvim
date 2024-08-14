5c1d6e771159b276b11d6e337c41f3eb4369c8c2
Samduykhanh01 <samduykhanh01@gmail.com>
2024-08-14 01:13:04 -0400

    Update Neovim configuration and data

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+--[[
+-- Require the jdtls module
+local jdtls = require("plugins.jdtls")
+-- Create an autocommand group
+vim.api.nvim_create_augroup("JDTLSSetup", { clear = true })
+-- Set up the autocommand to call setup_jdtls for Java files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function()
+    jdtls.setup_jdtls()
+  end,
+  group = "JDTLSSetup",
+})
+--]]
